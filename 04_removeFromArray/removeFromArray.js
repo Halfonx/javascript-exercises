509894036f248d92ce19c57afb993e51efb6775c
Halfonx <jermy.prigent@gmail.com>
2023-11-16 23:15:54 +0100

    Pass first test

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,8 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, argument) {
+    const filteredArray = array.filter(function (number) {
+        return number !== argument;
+    });
+    return filteredArray;
 };
 
 // Do not edit below this line
