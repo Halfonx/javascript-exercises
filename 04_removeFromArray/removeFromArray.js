c9b81b41d8909eeb72b49808029200f687d5b7d1
Halfonx <jermy.prigent@gmail.com>
2023-11-17 00:00:38 +0100

    Try and pass first test with another solution

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,9 +1,22 @@
-const removeFromArray = function(array, argument) {
-    const filteredArray = array.filter(function (number) {
-        return number !== argument;
-    });
-    return filteredArray;
+const removeFromArray = function(array, ...args) {
+    const newArray = [];
+    let arrayMaxIndex = array.length - 1;
+
+    for (let i = 0; i <= arrayMaxIndex; i++) {
+        if (array[i] != args) {
+            newArray.push(array[i]);
+        };
+    } return newArray;
 };
 
 // Do not edit below this line
 module.exports = removeFromArray;
+
+/*
+Cette fonction reçoit plusieurs arguments, en premier un tableau suivi d'un ou plusieurs nombre
+La fonction doit retourner un tableau identique à celui fourni sans les nombres indiqués en argument
+
+On parcours le tableau
+Si on tombe sur une valeur identique à l'un des arguments
+On enlève 
+*/
