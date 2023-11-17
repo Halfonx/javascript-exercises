1c4eb80eb2a551d4106c6230713a2fdbe27b7063
Halfonx <jermy.prigent@gmail.com>
2023-11-17 23:02:20 +0100

    Solve all the tests and add comments about the function

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,6 @@
 const removeFromArray = function(array, ...args) {
-    const newArray = array.filter((element) => !args.includes(element));
+    const newArray = array.filter((element) => !args.includes(element)); // This function filters and stores all the element that are not in the args in a new array
+    // args acts like an array
     return newArray;
 };
 
