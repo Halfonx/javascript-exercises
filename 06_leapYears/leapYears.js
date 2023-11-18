60ac9f7ed20f35a4ac703d67ce9d43c0930d619a
Halfonx <jermy.prigent@gmail.com>
2023-11-18 23:18:31 +0100

    Solve all the tests

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,18 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    if (year % 400 == 0) {
+        return true;
+    } else if (year % 4 == 0 && !Number.isInteger(year / 100)) {
+        return true;
+    } else {
+        return false;
+    }
 };
 
 // Do not edit below this line
 module.exports = leapYears;
+
+/*
+on me fournit un chiffre
+pour être vrai, il doit être divisible par 4
+mais les siècles doivent être vrai que si on peut les diviser par 400
+*/
