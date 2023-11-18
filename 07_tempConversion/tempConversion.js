76b2aff8d52fed3360d4238db2ff41e990ebd7e2
Halfonx <jermy.prigent@gmail.com>
2023-11-18 23:44:04 +0100

    Pass all the tests

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temperature) {
+  let celsiusTemperature = (temperature - 32) * (5 / 9);
+   celsiusTemperature = parseFloat(celsiusTemperature = celsiusTemperature.toFixed(1));
+  return celsiusTemperature;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temperature) {
+  let farenheitTemperature = (temperature * (9/5)) + 32;
+  farenheitTemperature = parseFloat(farenheitTemperature = farenheitTemperature.toFixed(1));
+  return farenheitTemperature;
 };
 
 // Do not edit below this line
